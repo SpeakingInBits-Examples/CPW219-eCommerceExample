51d97e6656366981fef97568894b197acd5f2726
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-08-11 22:04:44 -0700

    Add RegistrationViewModel with validation properties
    Introduced a new `RegistrationViewModel` class that includes
    properties for `Username`, `Email`, `Password`, `ConfirmPassword`,
    and `DateOfBirth`. Added validation for `Username` to ensure
    alphanumeric characters, enforced length restrictions on
    `Password`, and implemented a comparison for `ConfirmPassword`
    to match `Password`. The `DateOfBirth` property is also included
    with appropriate data type annotations.

--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -39,3 +39,39 @@ public class Member
     /// </summary>
     public DateOnly DateOfBirth { get; set; }
 }
+
+public class RegistrationViewModel
+{
+    /// <summary>
+    /// Public facing username for the member.
+    /// Alphanumeric characters only
+    /// </summary>
+    [RegularExpression("^[a-zA-Z0-9]+$",
+        ErrorMessage = "Username must be alphanumeric only")]
+    [StringLength(25)]
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// Email for the Member
+    /// </summary>
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// The member's password
+    /// </summary>
+    [StringLength(50, MinimumLength = 6,
+        ErrorMessage = "Your password must be between 6 and 50 characters")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
+
+    /// <summary>
+    /// The date of birth of the member
+    /// </summary>
+    [DataType(DataType.Date)]
+    public DateOnly DateOfBirth { get; set; }
+}
