17f01b20ec5a8cd66996c5c72c98cf3c3d67b535
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-08-11 21:02:51 -0700

    Add Member class for user representation in eCommerce
    Introduces the `Member` class in the `eCommerce.Models` namespace.
    This class includes properties for `MemberId`, `Username`, `Email`,
    `Password`, and `DateOfBirth`, with validation attributes for required
    fields and a primary key. The `DateOfBirth` uses `DateOnly` for date
    representation without time.

---
+++ eCommerce/Models/Member.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models;
+
+/// <summary>
+/// Represents an individual website user
+/// </summary>
+public class Member
+{
+    /// <summary>
+    /// Unique identifier for the member
+    /// </summary>
+    [Key]
+    public int MemberId { get; set; }
+
+    /// <summary>
+    /// Public facing username for the member.
+    /// Alphanumeric characters only
+    /// </summary>
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// Email for the Member
+    /// </summary>
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// The member's password
+    /// </summary>
+    public required string Password { get; set; }
+
+    /// <summary>
+    /// The date of birth of the member
+    /// </summary>
+    public DateOnly DateOfBirth { get; set; }
+}
